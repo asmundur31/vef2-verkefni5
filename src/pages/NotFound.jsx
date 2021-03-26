f1cf92065945bc466479123ae34df8041158900f
Ásmundur Óskar Ásmundsson <asmundur31@gmail.com>
2021-03-26 18:41:53 +0000

    bætti við til baka link á notFound síðu

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function NotFound() {
   return (
-    <p>404 villa - Fannst ekki</p>
+    <div>
+<p>404 villa - Fannst ekki</p>
+    <p><Link to='/'>Til baka</Link></p>
+    </div>
+    
   );
 }
